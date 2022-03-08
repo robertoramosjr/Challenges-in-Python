c0c35eee0e65a8bcdd3724cec3619d88c9a28b85
robertoramosjr <robeerto.aguiar.ramos@gmail.com>
2022-03-08 14:18:56 -0300

    4 - script that generates a random CPF

---
+++ Desafio#1/CPF-maker.py
@@ -0,0 +1,78 @@
+from random import randint
+
+
+def turn_in_list_of_int():
+    global random_number_in_list, random_number_in_int_list
+    for n in random_number_in_list:
+        int_n = int(n)
+        random_number_in_int_list.append(int_n)
+
+
+def eleventh_digit_validation_calculus():
+    global sum_eleventh_digit
+    return 11 - (sum_eleventh_digit % 11)
+
+
+def tenth_digit_validation_calculus():
+    global sum_tenth_digit
+    return 11 - (sum_tenth_digit % 11)
+
+
+def define_digit_ten():
+    return 0 if tenth_digit_validation > 9 else tenth_digit_validation
+
+
+def define_digit_eleven():
+    return 0 if eleventh_digit_validation > 9 else eleventh_digit_validation
+
+
+def calculate_ten_digit():
+    global sum_tenth_digit, x
+    while x <= 8:
+        multiplied_number = random_number_in_int_list[x] * factors_to_check_tenth_digit[x]
+        sum_tenth_digit += multiplied_number
+        x += 1
+    return sum_tenth_digit
+
+
+def calculate_eleven_digit():
+    global sum_eleventh_digit, y
+    while y <= 9:
+        new_multiplied_number = random_number_in_int_list[y] * factor_to_check_eleventh_digit[y]
+        sum_eleventh_digit += new_multiplied_number
+        y += 1
+    return sum_eleventh_digit
+
+
+sum_tenth_digit = 0
+x = 0
+sum_eleventh_digit = 0
+y = 0
+factor_to_check_eleventh_digit = list(range(11, 1, -1))
+factors_to_check_tenth_digit = list(range(10, 1, -1))
+
+
+random_number = str(randint(100000000, 999999999))
+random_number_in_list = list(random_number)
+random_number_in_int_list = []
+
+for number in random_number_in_list:
+    number_as_int = int(number)
+    random_number_in_int_list.append(number_as_int)
+
+sum_tenth_digit = calculate_ten_digit()
+
+tenth_digit_validation = tenth_digit_validation_calculus()
+
+digit_ten = define_digit_ten()
+
+random_number_in_int_list.append(digit_ten)
+
+sum_eleventh_digit = calculate_eleven_digit()
+
+eleventh_digit_validation = eleventh_digit_validation_calculus()
+
+digit_eleven = define_digit_eleven()
+
+print(random_number + str(digit_ten) + str(digit_eleven))
+
